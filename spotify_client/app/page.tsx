86cf31f60115d4c670d52c6d515464d02e8bc2a7
0HH02 <carloschang710@gmail.com>
2025-02-15 23:15:22 -0500

    recovering the progress in the frontend part 1

--- spotify_client/app/page.tsx
+++ spotify_client/app/page.tsx
@@ -1,11 +1,11 @@
 "use client";
 
-import { useEffect, useState, useCallback } from 'react';
-import axios from 'axios';
-import { RetroMusicPlayer } from '@/components/RetroMusicPlayer';
-import { SongList } from '@/components/SongList';
-import { Menu } from 'lucide-react'; // Importar ArrowRight
-import serverManager from '@/middleware/ServerManager';
+import { useEffect, useState, useCallback } from "react";
+import axios from "axios";
+import { RetroMusicPlayer } from "@/components/RetroMusicPlayer";
+import { SongList } from "@/components/SongList";
+import { Menu } from "lucide-react"; // Importar ArrowRight
+import serverManager from "@/middleware/ServerManager";
 
 interface Song {
   id: number;
@@ -13,14 +13,19 @@ interface Song {
   artist: string;
   genre: string;
   album: string;
+  image: string;
+  duration: number;
+  size: number;
 }
 
 export default function Home() {
   const [songs, setSongs] = useState([]);
   const [currentSongId, setCurrentSongId] = useState<string | null>(null);
   const [loading, setLoading] = useState(true);
-  const [sortType, setSortType] = useState<'all' | 'artist' | 'genre' | 'album'>('all');
-  const [searchTerm, setSearchTerm] = useState('');
+  const [sortType, setSortType] = useState<
+    "all" | "artist" | "genre" | "album"
+  >("all");
+  const [searchTerm, setSearchTerm] = useState("");
   const [showSongList, setShowSongList] = useState(true); // Inicialmente visible
 
   useEffect(() => {
@@ -38,12 +43,15 @@ export default function Home() {
         artist: song.artist,
         genre: song.genre,
         album: song.album,
-        coverUrl: 'default_image.jpg',
+        coverUrl: `${server}${song.image}`,
+        duration: song.duration,
+        fileSize: song.size,
       }));
+      console.log(apiSongs);
       setSongs(apiSongs);
       if (apiSongs.length > 0) setCurrentSongId(apiSongs[0].id);
     } catch (error) {
-      console.error('Error fetching songs:', error);
+      console.error("Error fetching songs:", error);
     } finally {
       setLoading(false);
     }
@@ -52,10 +60,12 @@ export default function Home() {
   const fetchSongsBySearch = async (type: string, term: string) => {
     setLoading(true);
     try {
-      const searchBy = type === 'all' ? 'title' : type;
+      const searchBy = type === "all" ? "title" : type;
       const server = await serverManager.getAvailableServer();
       const response = await axios.get(
-        `${server}/api/search/?searchBy=${searchBy}&query=${encodeURIComponent(term)}`
+        `${server}/api/search/?searchBy=${searchBy}&query=${encodeURIComponent(
+          term
+        )}`
       );
       const apiSongs = response.data.data.map((song: Song) => ({
         id: song.id.toString(),
@@ -63,12 +73,12 @@ export default function Home() {
         artist: song.artist,
         genre: song.genre,
         album: song.album,
-        coverUrl: 'default_image.jpg',
+        coverUrl: "default_image.jpg",
       }));
       setSongs(apiSongs);
       if (apiSongs.length > 0) setCurrentSongId(apiSongs[0].id);
     } catch (error) {
-      console.error('Error searching songs:', error);
+      console.error("Error searching songs:", error);
     } finally {
       setLoading(false);
     }
@@ -76,7 +86,7 @@ export default function Home() {
 
   const handleSearchCriteriaChange = useCallback(
     (newSortType: string, newSearchTerm: string) => {
-      if (newSearchTerm.trim() === '') {
+      if (newSearchTerm.trim() === "") {
         fetchAllSongs();
       } else {
         fetchSongsBySearch(newSortType, newSearchTerm);
@@ -85,7 +95,7 @@ export default function Home() {
     []
   );
 
-  const updateSortType = (type: 'all' | 'artist' | 'genre' | 'album') => {
+  const updateSortType = (type: "all" | "artist" | "genre" | "album") => {
     setSortType(type);
     handleSearchCriteriaChange(type, searchTerm);
   };
@@ -107,22 +117,22 @@ export default function Home() {
           <Menu size={24} />
         </button>
       )}
-      
+
       {/* Contenedor de la lista de canciones */}
       <div
         className={`
           fixed top-0 left-0 h-full bg-purple-900 transform transition-transform duration-300 
-          ${showSongList ? 'translate-x-0' : '-translate-x-full'} 
+          ${showSongList ? "translate-x-0" : "-translate-x-full"} 
           md:w-80 w-64 z-40
         `}
       >
         <SongList
           songs={songs}
-          currentSongId={currentSongId || ''}
+          currentSongId={currentSongId || ""}
           onSongSelect={setCurrentSongId}
           sortType={sortType}
           searchTerm={searchTerm}
-          onSortTypeChange={updateSortType }
+          onSortTypeChange={updateSortType}
           onSearchTermChange={updateSearchTerm}
           onSongUpload={(file) => {
             // Implementa aquí la lógica de subir canción
@@ -136,10 +146,14 @@ export default function Home() {
       </div>
 
       {/* Contenedor del Reproductor, ajusta el margen izquierdo según la visibilidad del slider */}
-      <div className={`flex-1 flex justify-center items-center p-4 transition-all duration-300 ${showSongList ? 'md:ml-80' : 'md:ml-0'}`}>
+      <div
+        className={`flex-1 flex justify-center items-center p-4 transition-all duration-300 ${
+          showSongList ? "md:ml-80" : "md:ml-0"
+        }`}
+      >
         <RetroMusicPlayer
           songs={songs}
-          currentSongId={currentSongId || ''}
+          currentSongId={currentSongId || ""}
           onSongSelect={setCurrentSongId}
           loading={loading}
         />
--- spotify_client/components/SongList.tsx
+++ spotify_client/components/SongList.tsx
@@ -1,8 +1,16 @@
-import React, { useState, useRef } from 'react';
-import { motion } from 'framer-motion';
-import { Search, Music, User, Disc, Album, Upload, ArrowLeft } from 'lucide-react'; // Importar ArrowLeft
-import serverManager from '@/middleware/ServerManager';
-import Image from 'next/image';
+import React, { useState, useRef } from "react";
+import { motion } from "framer-motion";
+import {
+  Search,
+  Music,
+  User,
+  Disc,
+  Album,
+  Upload,
+  ArrowLeft,
+} from "lucide-react"; // Importar ArrowLeft
+import serverManager from "@/middleware/ServerManager";
+import Image from "next/image";
 
 interface Song {
   id: string;
@@ -20,12 +28,12 @@ interface SongListProps {
   onSongUpload: (file: File) => void;
   sortType: string;
   searchTerm: string;
-  onSortTypeChange: (type: 'all' | 'artist' | 'genre' | 'album') => void;
+  onSortTypeChange: (type: "all" | "artist" | "genre" | "album") => void;
   onSearchTermChange: (term: string) => void;
   onHide: () => void; // Nueva prop para ocultar la lista
 }
 
-type SortType = 'all' | 'artist' | 'genre' | 'album';
+type SortType = "all" | "artist" | "genre" | "album";
 
 export const SongList: React.FC<SongListProps> = ({
   songs,
@@ -41,31 +49,35 @@ export const SongList: React.FC<SongListProps> = ({
   const [isDragging, setIsDragging] = useState(false);
   const fileInputRef = useRef<HTMLInputElement>(null);
 
-  const handleFileChange = async (event: React.ChangeEvent<HTMLInputElement>) => {
+  const handleFileChange = async (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
     const file = event.target.files?.[0];
-    if (file && file.type === 'audio/mpeg') {
+    if (file && file.type === "audio/mpeg") {
       try {
         const formData = new FormData();
-        formData.append('file', file); // 'file' es el nombre esperado por el servidor
+        formData.append("file", file); // 'file' es el nombre esperado por el servidor
         const server = await serverManager.getAvailableServer();
         const response = await fetch(`${server}/api/upload/`, {
-          method: 'POST',
+          method: "POST",
           body: formData,
         });
-  
+
         if (response.ok) {
-          alert('Archivo subido correctamente.');
+          alert("Archivo subido correctamente.");
           onSongUpload(file); // Actualiza el estado local
         } else {
           const errorData = await response.json();
-          alert(`Error en la subida: ${errorData.message || 'Error desconocido'}`);
+          alert(
+            `Error en la subida: ${errorData.message || "Error desconocido"}`
+          );
         }
       } catch (error) {
-        console.error('Error al subir el archivo:', error);
-        alert('Ocurrió un error al subir el archivo.');
+        console.error("Error al subir el archivo:", error);
+        alert("Ocurrió un error al subir el archivo.");
       }
     } else {
-      alert('Por favor, selecciona un archivo MP3 válido.');
+      alert("Por favor, selecciona un archivo MP3 válido.");
     }
   };
 
@@ -82,19 +94,27 @@ export const SongList: React.FC<SongListProps> = ({
     event.preventDefault();
     setIsDragging(false);
     const file = event.dataTransfer.files[0];
-    if (file && file.type === 'audio/mpeg') {
+    if (file && file.type === "audio/mpeg") {
       onSongUpload(file);
     } else {
-      alert('Por favor, suelta un archivo MP3 válido.');
+      alert("Por favor, suelta un archivo MP3 válido.");
     }
   };
 
-  const SortIcon = ({ type, icon: Icon }: { type: SortType; icon: React.ElementType }) => (
+  const SortIcon = ({
+    type,
+    icon: Icon,
+  }: {
+    type: SortType;
+    icon: React.ElementType;
+  }) => (
     <motion.div
       whileHover={{ scale: 1.1 }}
       whileTap={{ scale: 0.9 }}
       onClick={() => onSortTypeChange(type)}
-      className={`cursor-pointer p-2 rounded-full ${sortType === type ? 'bg-purple-600' : 'bg-purple-800'}`}
+      className={`cursor-pointer p-2 rounded-full ${
+        sortType === type ? "bg-purple-600" : "bg-purple-800"
+      }`}
     >
       <Icon size={20} className="text-white" />
     </motion.div>
@@ -122,7 +142,10 @@ export const SongList: React.FC<SongListProps> = ({
             onChange={(e) => onSearchTermChange(e.target.value)}
             className="w-full bg-purple-800 text-white placeholder-purple-300 border border-purple-600 rounded-md py-2 pl-10 pr-12 focus:outline-none focus:ring-2 focus:ring-purple-500"
           />
-          <Search className="absolute left-3 top-1/2 transform -translate-y-1/2 text-purple-300" size={20} />
+          <Search
+            className="absolute left-3 top-1/2 transform -translate-y-1/2 text-purple-300"
+            size={20}
+          />
         </div>
         <div className="flex justify-between mb-4">
           <SortIcon type="all" icon={Music} />
@@ -132,38 +155,44 @@ export const SongList: React.FC<SongListProps> = ({
         </div>
       </div>
       <div className="overflow-y-auto flex-grow hide-scrollbar px-6 pb-6">
-  {songs.map((song, index) => (
-    <motion.div
-      key={song.id}
-      initial={{ opacity: 0, y: 20 }}
-      animate={{ opacity: 1, y: 0 }}
-      transition={{ delay: index * 0.1 }}
-      className={`flex items-center space-x-4 p-3 rounded-lg cursor-pointer mb-4 ${
-        song.id === currentSongId ? 'bg-purple-700' : 'hover:bg-purple-800'
-      }`}
-      onClick={() => onSongSelect(song.id)}
-    >
-      <Image
-        src={song.coverUrl}
-        alt={song.title}
-        className="w-16 h-16 rounded-md object-cover"
-      />
-      <div className="flex-grow overflow-hidden">
-        <p className="text-white font-semibold truncate w-full whitespace-nowrap overflow-hidden text-ellipsis">
-          {song.title}
-        </p>
-        <p className="text-purple-300 text-sm truncate w-full whitespace-nowrap overflow-hidden text-ellipsis">
-          {song.artist}
-        </p>
-        <p className="text-purple-400 text-xs truncate w-full whitespace-nowrap overflow-hidden text-ellipsis">
-          {sortType === 'genre' ? song.genre : song.album}
-        </p>
+        {songs.map((song, index) => (
+          <motion.div
+            key={song.id}
+            initial={{ opacity: 0, y: 20 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ delay: index * 0.1 }}
+            className={`flex items-center space-x-4 p-3 rounded-lg cursor-pointer mb-4 ${
+              song.id === currentSongId
+                ? "bg-purple-700"
+                : "hover:bg-purple-800"
+            }`}
+            onClick={() => onSongSelect(song.id)}
+          >
+            <Image
+              src={song.coverUrl}
+              alt={song.title}
+              width={480}
+              height={480}
+              className="w-16 h-16 rounded-md object-cover"
+            />
+            <div className="flex-grow overflow-hidden">
+              <p className="text-white font-semibold truncate w-full whitespace-nowrap overflow-hidden text-ellipsis">
+                {song.title}
+              </p>
+              <p className="text-purple-300 text-sm truncate w-full whitespace-nowrap overflow-hidden text-ellipsis">
+                {song.artist}
+              </p>
+              <p className="text-purple-400 text-xs truncate w-full whitespace-nowrap overflow-hidden text-ellipsis">
+                {sortType === "genre" ? song.genre : song.album}
+              </p>
+            </div>
+          </motion.div>
+        ))}
       </div>
-    </motion.div>
-  ))}
-</div>
       <div
-        className={`p-4 border-t border-purple-700 ${isDragging ? 'bg-purple-700' : 'bg-purple-800'}`}
+        className={`p-4 border-t border-purple-700 ${
+          isDragging ? "bg-purple-700" : "bg-purple-800"
+        }`}
         onDragOver={handleDragOver}
         onDragLeave={handleDragLeave}
         onDrop={handleDrop}
@@ -181,7 +210,7 @@ export const SongList: React.FC<SongListProps> = ({
           className="flex items-center justify-center cursor-pointer text-white hover:text-purple-300 transition-colors"
         >
           <Upload size={20} className="mr-2" />
-          <span>{isDragging ? 'Suelta el MP3 aquí' : 'Subir MP3'}</span>
+          <span>{isDragging ? "Suelta el MP3 aquí" : "Subir MP3"}</span>
         </label>
       </div>
     </div>
@@ -198,8 +227,8 @@ const hideScrollbarStyle = `
   }
 `;
 
-if (typeof document !== 'undefined') {
-  const style = document.createElement('style');
+if (typeof document !== "undefined") {
+  const style = document.createElement("style");
   style.textContent = hideScrollbarStyle;
   document.head.appendChild(style);
 }
