3f8b0ddc155d327f334994e9227cb1cc050ded6a
karlosgr <karlosgr0201@gmail.com>
2025-02-25 10:35:08 -0500

    - added more control and verification logs

--- spotify_server/spotify/logs.py
+++ spotify_server/spotify/logs.py
@@ -3,7 +3,8 @@ import threading
 lock = threading.Lock()
 
 
-def write_log(log: str) -> None:
+def write_log(log: str, t=0) -> None:
+    log_file = "c_logs.txt" if t == 0 else "k_logs.txt"
     with lock:
-        with open("./logs/logs.txt", "a") as f:
+        with open(f"./logs/{log_file}", "a", encoding="utf-8") as f:
             f.write(log + "\n")
